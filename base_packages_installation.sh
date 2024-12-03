1f52186a6458fdce254db9f45c5339b5f6a5beb2
Danil Vashukov <dvashukov1@yandex.ru>
2024-12-03 22:11:00 +0300

    added base config installation

--- base_packages_installation.sh
+++ base_packages_installation.sh
@@ -1,1 +1,1 @@
-sudo pacman -S --needed wlsunset git base-devel && git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si
+cp -r ./config/ ~/.config/ && sudo pacman -S --needed wlsunset git base-devel && git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si
